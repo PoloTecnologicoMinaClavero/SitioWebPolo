e3ab7586f661675f444a2ab880e0535b03ab42c7
ezefdeil <ezequiel.fdeil@gmail.com>
2021-03-12 14:49:25 -0300

    #11 subimos primer avance Seccion empresas

---
+++ PoloSitioWeb/PoloSitioWeb/Entities/Enterprise.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoloSitioWeb.Entities
+{
+    public class Enterprise
+    {
+        public string Name { get; set; }
+        public string SiteUrl { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageDescription { get; set; }
+    }
+}
---
+++ PoloSitioWeb/PoloSitioWeb/Helpers/EnterprisesHelper.cs
@@ -0,0 +1,41 @@
+﻿using PoloSitioWeb.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoloSitioWeb.Helpers
+{
+    public static class EnterprisesHelper
+    {
+        public static List<Enterprise> GetAll()
+        {
+            var enterprises = new List<Enterprise>();
+
+            enterprises.Add(new Enterprise()
+            {
+                Name = "IncluIT",
+                SiteUrl = "https://incluit.com",
+                ImageUrl = "/assets/img/logos/Incluit.svg",
+                ImageDescription = "IncluIT Logo"
+            });
+            enterprises.Add(new Enterprise()
+            {
+                Name = "Verne",
+                SiteUrl = "https://www.vernegroup.com/",
+                ImageUrl = "/assets/img/logos/Verne.png",
+                ImageDescription = "Verne Logo"
+            });
+            enterprises.Add(new Enterprise()
+            {
+                Name = "Mantis",
+                SiteUrl = "http://www.mantis.com.ar/",
+                ImageUrl = "/assets/img/logos/Mantis-Azul.png",
+                ImageDescription = "Mantis Logo"
+            });
+
+
+            return enterprises;
+        }
+    }
+}
