103d699e243c87e6217f2627fa6dc3d96f094550
tomas ignacio pereyra <tomaspereyra1997@gmail.com>
2021-03-16 23:18:13 -0300

    se modificaron variables en javascript

--- PoloSitioWeb/PoloSitioWeb/wwwroot/scripts/Contacto/binding.js
+++ PoloSitioWeb/PoloSitioWeb/wwwroot/scripts/Contacto/binding.js
@@ -2,17 +2,18 @@
 
 class ContactoRequest {
     constructor(data) {
-        this.NombreApellido = data?.NombreApellido;
-        this.Email = data?.Email;
-        this.Asunto = data?.Asunto;
-        this.Mensaje = data?.Mensaje;
+        this.NombreApellido = !data?.NombreApellido ? "" : data.NombreApellido ;
+        this.Email = !data?.Email ? "" : data.Email;
+        this.Asunto = !data?.Asunto ? "" : data.Asunto;
+        this.Mensaje = !data?.Mensaje ? "" : data.Mensaje;
     }
     GetJson() {
         return JSON.stringify(this);
     }
-    static CleanPropeties(data) {
-        for (const key in new ContactoRequest({}))
-            data[key] = "";
+    static CleanProperties(data) {
+        const item = new ContactoRequest();
+        for (const key in item)
+            data[key] = item[key];
     }
 }
 
@@ -39,13 +40,11 @@ const vm = new Vue({
             axios.post(url + "/SendMailAsync", contacto)
                 .then(() => {
                     this.p_SuccessMessage = "Su mensaje ha sido enviado!";
-                    ContactoRequest.CleanPropeties(vm.$data);
+                    ContactoRequest.CleanProperties(vm.$data);
                 })
                 .catch(error => {
-
                     console.error(error);
                     this.p_ErrorMessage = error.message;
-
                 }).then(() => {
                     this.p_DisabledForm = false;
                 });
