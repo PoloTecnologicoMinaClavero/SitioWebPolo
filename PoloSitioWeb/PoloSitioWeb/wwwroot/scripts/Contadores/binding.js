ed0dafefe2d3a4a096337adaa25d1f5277d35348
tomas ignacio pereyra <tomaspereyra1997@gmail.com>
2021-04-09 12:13:25 -0300

    DEV- Cambiar contadores #28

--- PoloSitioWeb/PoloSitioWeb/wwwroot/scripts/Contadores/binding.js
+++ PoloSitioWeb/PoloSitioWeb/wwwroot/scripts/Contadores/binding.js
@@ -5,18 +5,18 @@ const counters = new Vue({
         CandtidadTrabajando: 1,
         CandtidadProyectos: 1,
         CandtidadCursosDictados: 1,
-        DiasExistenciaPolo: 1
+        Empresas: 1
     },
     methods: {
         GetCounters: function () {
             axios.get(UrlApiHome + "/GetCounters").then((response) => {
                 if (response && response.data) {
                     var x = response.data;
-                    console.log(x);
+
                     this.CandtidadTrabajando = x.CandtidadTrabajando;
                     this.CandtidadProyectos = x.CandtidadProyectos;
                     this.CandtidadCursosDictados = x.CandtidadCursosDictados;
-                    this.DiasExistenciaPolo = x.DiasExistenciaPolo;
+                    this.Empresas = x.Empresas;
                 }
             });
         }
