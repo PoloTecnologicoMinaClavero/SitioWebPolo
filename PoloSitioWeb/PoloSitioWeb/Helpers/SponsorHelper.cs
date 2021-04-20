b53eb0d1b7bccd00f75b525d0d180a2444f6a366
ezefdeil <ezequiel.fdeil@gmail.com>
2021-04-20 15:09:40 -0300

    Correcciones en sponsor

--- PoloSitioWeb/PoloSitioWeb/Helpers/SponsorHelper.cs
+++ PoloSitioWeb/PoloSitioWeb/Helpers/SponsorHelper.cs
@@ -18,7 +18,7 @@ namespace PoloSitioWeb.Helpers
                 SiteUrl = "https://leistungargentina.com.ar/",
                 ImageUrl = "/assets/img/logos/Leistung.png",
                 ImageDescription = "Leistung Logo",
-                Description = "\"Leistung Argentina es una empresa que crea dispositivos médicos para ventilación mecánica, de muchísima importancia en estos tiempos de pandemia. Es nuestra empresa patrocinadora. Nuestros padrinos.  Han hecho una importante donación de 10 computadores para el Polo Tecnológico y 3 equipos para la Escuela Proa de Mina Clavero. Todos equipos de última generación. Además gracias a su aporte financiero, es que podremos ofrecer las capacitaciones del año 2021 en forma totalmente gratuita para el público en general.\""
+                Description = "Leistung Argentina es una empresa que crea dispositivos médicos para ventilación mecánica, de muchísima importancia en estos tiempos de pandemia. Es nuestra empresa patrocinadora. Nuestros padrinos.  Han hecho una importante donación de 10 computadores para el Polo Tecnológico y 3 equipos para la Escuela Proa de Mina Clavero. Todos equipos de última generación. Además gracias a su aporte financiero, es que podremos ofrecer las capacitaciones del año 2021 en forma totalmente gratuita para el público en general."
             });
             
 
